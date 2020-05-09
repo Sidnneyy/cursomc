d9d45f9de85fefab582d215c85c846e3099d4dcb
Sidney <tecnicsidney@gmail.com>
2020-05-09 11:24:51 -0300

    Banco de dados H2 de teste e criação automatica base de dados apartida da class de dominio categoria

--- src/main/java/com/sidney/cursomc/domain/Categoria.java
+++ src/main/java/com/sidney/cursomc/domain/Categoria.java
@@ -2,11 +2,20 @@ package com.sidney.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
-
-	private Integer id;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "Nome")
 	public String nome;
 
 	public Categoria() {
