560edc151fb35ccde6a7aa48a8e1c7ef936830f2
Sidney <tecnicsidney@gmail.com>
2020-05-11 10:31:23 -0300

    Criação metodo para instanciar class categoria e grava os objetos no banco

--- src/main/java/com/sidney/cursomc/CursomcApplication.java
+++ src/main/java/com/sidney/cursomc/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.sidney.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.hibernate.mapping.Array;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.sidney.cursomc.domain.Categoria;
+import com.sidney.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Sala");
+		Categoria cat2 = new Categoria(null, "Quarto");
+		Categoria cat3 = new Categoria(null, "Galpão");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 
 }
